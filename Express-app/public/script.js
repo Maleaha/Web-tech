ebe7f493effc3e37e4e57ac2379d71d65a7f5457
Maleaha <maleahawork@gmail.com>
2025-10-10 18:59:46 +0500

    .

---
+++ Express-app/public/script.js
@@ -0,0 +1,2 @@
+//public/script.js
+console.log("hey there");
--- Express-app/server.js
+++ Express-app/server.js
@@ -1,15 +1,27 @@
-const express = require('express');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
 const app = express();
 const port = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Set EJS as view engine
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(expressLayouts);
+
+// Set default layout
+app.set("layout", "layout");
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("homepage", { title: "Home" });
 });
 
-app.get('/contact', (req, res) => {
-  res.send('Contact me at: ');
+app.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact" });
 });
 
+// Start server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`✅ Server is running on http://localhost:${port}`);
 });
