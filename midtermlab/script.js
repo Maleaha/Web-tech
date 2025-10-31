b06deb4646a0bfced880fe936f816560fb6f9538
Maleaha <maleahawork@gmail.com>
2025-10-31 10:53:06 +0500

    mid term lab

---
+++ midtermlab/script.js
@@ -0,0 +1,44 @@
+$(function() {
+  $('#contactForm').submit(function(e) {
+    e.preventDefault();
+    
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var subject = $('#subject').val();
+    var message = $('#message').val();
+
+    $(".invalid-feedback").remove();
+    $("input, textarea").removeClass("is-invalid");
+    var valid = true;
+
+    if (name.trim() === "" || name.length < 3) {
+      $("#name").addClass("is-invalid");
+      $("#name").after("<div class='invalid-feedback'>Try again (min 3 letters)</div>");
+      valid = false;
+    }
+
+    if (email.trim() === "" || !email.includes("@")) {
+      $("#email").addClass("is-invalid");
+      $("#email").after("<div class='invalid-feedback'>Enter a valid email</div>");
+      valid = false;
+    }
+
+    if (subject.trim() === "") {
+      $("#subject").addClass("is-invalid");
+      $("#subject").after("<div class='invalid-feedback'>Enter a subject</div>");
+      valid = false;
+    }
+
+    if (message.trim() === "") {
+      $("#message").addClass("is-invalid");
+      $("#message").after("<div class='invalid-feedback'>Enter your message</div>");
+      valid = false;
+    }
+
+    if (valid) {
+      alert("Form submitted successfully!");
+      $("#contactForm").trigger("reset");
+    }
+  });
+
+});
